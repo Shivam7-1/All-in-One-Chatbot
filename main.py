b8dbe1f1e1d3ff07ffbabd5c661abfd5ed130f2f
Shivam7-1 <55046031+Shivam7-1@users.noreply.github.com>
2023-05-06 11:55:07 +0530

    Main files

---
+++ main.py
@@ -0,0 +1,58 @@
+
+import os
+import openai
+import gradio as gr
+
+#if you have OpenAI API key as an environment variable, enable the below
+#openai.api_key = os.getenv("OPENAI_API_KEY")
+
+#if you have OpenAI API key as a string, enable the below
+openai.api_key = "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"
+
+start_sequence = "\nAI:"
+restart_sequence = "\nHuman: "
+
+prompt = " "
+
+def openai_create(prompt):
+
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=prompt,
+    temperature=0.9,
+    max_tokens=150,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0.6,
+    stop=[" Human:", " AI:"]
+    )
+
+    return response.choices[0].text
+
+
+
+def txt(input, history):
+    history = history or []
+    s = list(sum(history, ()))
+    s.append(input)
+    inp = ' '.join(s)
+    output = openai_create(inp)
+    history.append((input, output))
+    return history, history
+
+
+block = gr.Blocks()
+
+
+with block:
+    gr.Markdown("""<h1><center>All In one chatbot</center></h1>
+    """)
+    chatbot = gr.Chatbot()
+    message = gr.Textbox(placeholder=prompt)
+    state = gr.State()
+    submit = gr.Button("SEND")
+    submit.click(txt, inputs=[message, state], outputs=[chatbot, state])
+
+block.launch(share = True)
+link = block.launch(share=True)
+print("Shareable link:", link)
